2442138d864e7a2e9920bc10fc0e18beb1f79ea9
MinxingFu <129235714+MinxingFu@users.noreply.github.com>
2023-06-05 21:39:18 +0800

    Update dependencies.py

--- code/test/assets/dependencies.py
+++ code/test/assets/dependencies.py
@@ -1,14 +1,9 @@
 import numpy as np
 import pandas as pd
 import math
-import requests
-import datetime
-from pysolar.solar import get_altitude, get_azimuth
-import pytz
 import csv
 from mpl_toolkits.mplot3d import Axes3D
 from bokeh.io import output_notebook
-import os 
 from bokeh.io import show
 from bokeh.plotting import gmap
 from bokeh.models import GMapOptions
